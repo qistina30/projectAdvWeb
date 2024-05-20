7855f31b905a75ed44153e8eb21dfbbccf8d1b4f
qistina <naqistina00@gmail.com>
2024-05-20 23:50:06 +0800

    function 2 90%

---
+++ app/Models/Member.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\SoftDeletes;
+
+class Member extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function volunteer()
+    {
+        return $this->belongsTo(Volunteer::class);
+    }
+}
